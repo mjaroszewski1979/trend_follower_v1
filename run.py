506d2d3fc8c566dd083b549d498da4126ac7c5a9
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-09-24 21:07:38 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,20 +4,24 @@ from extensions import db, login_manager
 
 
 
-
+# Creating an application object using factory pattern
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
 
+    # Loading settings
     app.config.from_pyfile(config_file)
 
+    # Initializing extensions
     db.init_app(app)
     login_manager.init_app(app)
 
+    # Creating neccesary application context
     with app.app_context():
 
         from routes import main
 
+        # Registering main blueprint
         app.register_blueprint(main)
 
 
